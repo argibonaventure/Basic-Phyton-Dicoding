a2bb3241eaefb670fece84978a16484b033d50bf
argibonaventure <bonaventuragigi5@gmail.com>
2024-02-13 10:05:14 +0700

    add oneliner.py

---
+++ ekspresi.py
@@ -0,0 +1,26 @@
+#TYPES OF EXPRESSIONS
+
+#Based on Operator arity
+x=0
+y=0
+
+biner = x+y #Biner
+
+x+=1 #Uner
+
+
+#Based on the resulting data type
+
+# 2 + 2 = 4 (Arithmetic expression)
+
+# 3 < 1 = True , 1 < 10 = False (Relational expression)
+
+# True or False = False (Logical expression) 
+
+
+#TYPES OF OPERATORS
+
+#Arithmetic ( +, -, %, /, *, ** )
+#Relational ( < , >, <=, >=, ==, !=)
+#Logic (AND & , OR | , not)
+#Assignment ( += , -=, *=, /=, %= )
---
+++ oneliner.py
@@ -0,0 +1,23 @@
+x = 1
+y = 2
+
+temp = x
+x = y
+y = temp
+
+print("Setelah pertukaran: ")
+print("x = ", x)
+print("y =", y)
+
+
+
+#One-liner Method
+
+x = 1
+y = 2
+
+x, y = y, x    # One-liner
+
+print('Setelah pertukaran: ')
+print('x =', x)
+print('y =', y)
