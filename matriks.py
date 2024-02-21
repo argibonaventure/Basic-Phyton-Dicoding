0708beb5a70ba62883873ec56f11d68cd3585f9c
argibonaventure <bonaventuragigi5@gmail.com>
2024-02-21 10:26:59 +0700

    update matriks.py

--- matriks.py
+++ matriks.py
@@ -1,5 +1,6 @@
 import numpy
 import sys
+import numpy as np
 
 
 #Matrix in Python using list
@@ -34,3 +35,26 @@ var_mat = [[1, 2, 3, 4, 5],
            [21, 22, 23, 24, 25]]
            
 print(var_mat[2][1])
+
+print("======================")
+#Multiple calculation in Matrix
+
+#generic method, without library
+var_mat = [[5, 0],
+          [1, -2]]
+def_mat = [[0 for j in range(2)] for i in range(2)]
+
+for i in range(len(var_mat)):
+  for j in range(len(var_mat[0])):
+    def_mat[i][j] = var_mat[i][j]*2
+
+print(def_mat)
+
+
+#With numpy library
+
+var_mat2 = np.array([[5, 0],[1, -2]])
+
+result = var_mat2*2
+
+print (result)
